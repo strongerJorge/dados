4786465765fc531efe102a78524c57ddb77e76d5
strongerJorge <154372733+strongerJorge@users.noreply.github.com>
2023-12-19 17:06:37 -0300

    Add files via upload

---
+++ dados.py
@@ -0,0 +1,6 @@
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite  a sua idade: "))
+
+altura = float(input("Digite agora a sua altura: "))
+
+print(f"Olá {nome} pelo os dados que recebi seu, a sua idade é: {idade} e a sua altura é: {altura}")
